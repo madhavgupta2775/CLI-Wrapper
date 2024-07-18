2e88adf2f0883e22ba210dbff6adb30b373b8c2a
Srinath Murali <142419722+sensyble@users.noreply.github.com>
2024-07-18 16:46:32 +0530

    Hardcoded region and output format

--- shell_GUI.py
+++ shell_GUI.py
@@ -2,8 +2,20 @@ import ctypes
 from tkinter import *
 import os
 import sys
+import subprocess
+
 sys.path.insert(0, "./TkTerm")
 from tkterm import Terminal
+
+def configure_aws_cli():
+    # Set AWS region and output format
+    aws_region = "ap-south-1"
+    aws_output = "json"
+    
+    # Configure AWS CLI with the specified region and output format
+    subprocess.run(['aws', 'configure', 'set', 'region', aws_region])
+    subprocess.run(['aws', 'configure', 'set', 'output', aws_output])
+
 
 root = Tk()
 # Hide root window during initialization
@@ -17,13 +29,11 @@ term = Terminal(root)
 term.pack(expand=True, fill="both")
 
 # Set minimum size and center app
-
-# Update widgets so minimum size is accurate
 root.update_idletasks()
 
 # Get minimum size
-minimum_width: int = 800
-minimum_height: int = 600
+minimum_width = 800
+minimum_height = 600
 
 # Get center of screen based on minimum size
 x_coords = int(root.winfo_screenwidth() / 2 - minimum_width / 2)
@@ -33,6 +43,8 @@ y_coords = int(root.wm_maxsize()[1] / 2 - minimum_height / 2)
 root.geometry(f"{minimum_width}x{minimum_height}+{x_coords}+{y_coords}")
 root.wm_minsize(minimum_width, minimum_height)
 
+# Configure AWS CLI
+configure_aws_cli()
 
 # Show root window
 root.deiconify()
